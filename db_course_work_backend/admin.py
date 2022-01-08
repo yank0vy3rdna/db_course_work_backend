54113aabdc761d70286941e576d7f8be8a15a560
DeltaHeavyVIP <patutin.vovan@mail.ru>
2022-01-08 19:14:15 +0300

    Обновлен внешний вид admin panel

--- db_course_work_backend/admin.py
+++ db_course_work_backend/admin.py
@@ -43,15 +43,23 @@ class PLACE(admin.ModelAdmin):
 class EXHIBIT(admin.ModelAdmin):
     list_display = ("id",
                     "NAME",
-                    "AUTHOR",
+                    "Author",
                     "DATE_CREATION",
                     "DIRECTION",
-                    "LOCATION")
+                    "Location")
     search_fields = ("id",
-                     "NAME",
-                     "AUTHOR",
-                     "LOCATION")
+                     "NAME",)
     list_filter = ('AUTHOR', 'DATE_CREATION')
+
+    def Author(self, obj):
+        return obj.id
+
+    Author.short_description = "AUTHOR"
+
+    def Location(self, obj):
+        return obj.id
+
+    Location.short_description = "LOCATION"
 
 
 @admin.register(EXHIBITION)
@@ -61,9 +69,7 @@ class EXHIBITION(admin.ModelAdmin):
                     "Owner",
                     "Location")
     search_fields = ("id",
-                     "NAME",
-                     "OWNER",
-                     "LOCATION")
+                     "NAME",)
     list_filter = ('OWNER', 'LOCATION')
     filter_horizontal = ('exhibit',)
 
@@ -73,80 +79,165 @@ class EXHIBITION(admin.ModelAdmin):
     Owner.short_description = "OWNER"
 
     def Location(self, obj):
-        id = obj.id
-        # url = (
-        #     reverse("admin:db_course_work_backend_places_changelist")
-        # )
-        # return format_html('<a href="{}">ID_LOCATION {}</a>', url, id)
         return obj.id
 
     Location.short_description = "LOCATION"
 
 
-# todo
 @admin.register(MUSEUM)
 class MUSEUM(admin.ModelAdmin):
-    list_display = ("NAME",
-                    "LOCATION")
+    list_display = ("id",
+                    "NAME",
+                    "Location")
+    search_fields = ("id",
+                     "NAME",)
+    list_filter = ('LOCATION',)
+    filter_horizontal = ('exhibition',)
+
+    def Location(self, obj):
+        return obj.id
+
+    Location.short_description = "LOCATION"
 
 
 @admin.register(PASSPORT)
 class PASSPORT(admin.ModelAdmin):
-    list_display = (
-        "SERIES_PASSPORT",
-        "NUMBER_PASSPORT",
-        "SURNAME",
-        "NAME",
-        "PATRONYMIC",
-        "GENDER",
-        "DATE_BIRTHDAY",
-        "PASSPORT_ISSUE",
-        "DATE_ISSUE")
+    list_display = ("id",
+                    "SERIES_PASSPORT",
+                    "NUMBER_PASSPORT",
+                    "SURNAME",
+                    "NAME",
+                    "PATRONYMIC",
+                    "GENDER",
+                    "DATE_BIRTHDAY",
+                    "PASSPORT_ISSUE",
+                    "DATE_ISSUE")
+
+    search_fields = ("id",
+                     "SERIES_PASSPORT",
+                     "NUMBER_PASSPORT",
+                     "SURNAME",
+                     "NAME",
+                     "PATRONYMIC",
+                     "GENDER")
+    list_filter = ("DATE_BIRTHDAY",
+                   "PASSPORT_ISSUE",
+                   "DATE_ISSUE",)
 
 
 @admin.register(GUIDE)
 class GUIDE(admin.ModelAdmin):
-    list_display = ("MOBILE_NUMBER",
-                    "PASSPORT_ID",
+    list_display = ("id",
+                    "MOBILE_NUMBER",
+                    "PASSPORT_id",
                     "EMAIL")
+    search_fields = ("id",
+                     "MOBILE_NUMBER",
+                     "EMAIL",)
+
+    def PASSPORT_id(self, obj):
+        return obj.id
+
+    PASSPORT_id.short_description = "PASSPORT_ID"
 
 
 @admin.register(EXCURSIONIST)
 class EXCURSIONIST(admin.ModelAdmin):
-    list_display = ("HUMAN",
+    list_display = ("id",
+                    "Human",
                     "MOBILE_NUMBER",
                     "EMAIL")
+    search_fields = ("id",
+                     "MOBILE_NUMBER",
+                     "EMAIL",)
+
+    def Human(self, obj):
+        return obj.id
+
+    Human.short_description = "HUMAN"
 
 
 @admin.register(EXCURSION)
 class EXCURSION(admin.ModelAdmin):
-    list_display = ("NAME",
+    list_display = ("id",
+                    "NAME",
                     "DESCRIPTION",
                     "DURATION")
+    search_fields = ("id",
+                     "NAME",
+                     "DESCRIPTION",)
+    list_filter = ('DURATION',)
+    filter_horizontal = ('museum',
+                         'exhibition',
+                         'exhibit',)
 
 
 @admin.register(DOCUMENT_STATUS)
 class DOCUMENT_STATUS(admin.ModelAdmin):
-    list_display = ("INSTITUTION",
+    list_display = ("id",
+                    "INSTITUTION",
                     "NAME",
                     "DATE_ISSUE",
                     "DATE_CANCELLATION")
+    search_fields = ("id",
+                     "INSTITUTION",
+                     "NAME")
+    list_filter = ("DATE_ISSUE",
+                   "DATE_CANCELLATION",)
+    filter_horizontal = ('excursionist',)
 
 
 @admin.register(GROUP)
 class GROUP(admin.ModelAdmin):
-    list_display = ("EXHIBITION_ID",
-                    "GUIDE",
+    list_display = ("id",
+                    "EXHIBITION_id",
+                    "Guide",
                     "TIME",
                     "COST",
                     "NUMBER_SEATS",
-                    "PLACE_GATHERING",
-                    "PLACE_TERMINATION")
+                    "Place_gathering",
+                    "Place_termination")
+
+    search_fields = ("id",
+                     "TIME",
+                     "COST",
+                     "NUMBER_SEATS")
+    list_filter = ('PLACE_GATHERING',
+                   'PLACE_TERMINATION',)
+    filter_horizontal = ('excursionist',)
+
+    def EXHIBITION_id(self, obj):
+        return obj.id
+
+    EXHIBITION_id.short_description = "EXHIBITION_ID"
+
+    def Guide(self, obj):
+        return obj.id
+
+    Guide.short_description = "GUIDE"
+
+    def Place_gathering(self, obj):
+        return obj.id
+
+    Place_gathering.short_description = "PLACE_GATHERING"
+
+    def Place_termination(self, obj):
+        return obj.id
+
+    Place_termination.short_description = "PLACE_TERMINATION"
 
 
 @admin.register(DOCUMENT_ACCREDITATION)
 class DOCUMENT_ACCREDITATION(admin.ModelAdmin):
-    list_display = ("INSTITUTION",
+    list_display = ("id",
+                    "INSTITUTION",
                     "NAME",
                     "DATE_ISSUE",
                     "DATE_CANCELLATION")
+
+    search_fields = ("id",
+                     "INSTITUTION",
+                     "NAME",)
+    list_filter = ("DATE_ISSUE",
+                   "DATE_CANCELLATION",)
+    filter_horizontal = ('guide',)
