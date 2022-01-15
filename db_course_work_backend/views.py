ae14181d756f620cfdc78f87987719207881373c
DeltaHeavyVIP <patutin.vovan@mail.ru>
2022-01-15 19:57:06 +0300

    suka

--- db_course_work_backend/views.py
+++ db_course_work_backend/views.py
@@ -33,55 +33,22 @@ def view_list_excursions(request):
 
     # Получение определенной страницы с данными
     page_obj = get_page_object(exhibition_list, size, page_number)
-
-    return HttpResponse(page_obj)
+    resp_list = []
+    for num in range(0, len(page_obj.object_list)):
+        resp = return_json(page_obj.object_list[num].id)
+        if not (
+                resp == "<h2>Not Found EXCURSION</h2>" or resp == "<h2>Not Found Group With This EXCURSION</h2>"):
+            resp_list.append(json.loads(resp))
+    response = json.dumps(resp_list)
+    return HttpResponse(response)
 
 
 def view_excursion(request):
     # Получаем данные
-    id = request.GET.get("id", 1)
-
-    # Получаем данные и проверяем на существование
-    exhibition = EXCURSION.objects.filter(id=id)
-    museums = exhibition[0].museum.all()
-    expositions = exhibition[0].exhibition.all()
-    exhibits = exhibition[0].exhibit.all()
-    groups = GROUP.objects.filter(EXCURSION_ID=exhibition[0].id)
-
-    if not exhibition.exists():
-        return HttpResponseNotFound("<h2>Not Found EXHIBIT</h2>")
-
-    museums_list = []
-    for museum in museums:
-        museums_list.append(museum.NAME)
-
-    expositions_list = []
-    for exposition in expositions:
-        expositions_list.append(exposition.NAME)
-
-    exhibits_list = []
-    for exhibit in exhibits:
-        exhibits_list.append(exhibit.NAME)
-
-    fio = groups[0].GUIDE.PASSPORT_ID.SURNAME + " " + groups[0].GUIDE.PASSPORT_ID.NAME + " " + groups[
-        0].GUIDE.PASSPORT_ID.PATRONYMIC
-
-    group_list = []
-    for group in groups:
-        place_from = str(group.PLACE_GATHERING.COUNTRY) + " " + str(group.PLACE_GATHERING.CITY) + " " + str(
-            group.PLACE_GATHERING.STREET) + " " + str(group.PLACE_GATHERING.HOUSE)
-        place_to = str(group.PLACE_TERMINATION.COUNTRY) + " " + str(group.PLACE_TERMINATION.CITY) + " " + str(
-            group.PLACE_TERMINATION.STREET) + " " + str(group.PLACE_TERMINATION.HOUSE)
-        group_dictionary = {'date': str(group.TIME), 'free_slots': group.NUMBER_SEATS, 'id': group.id,
-                            'place_from': place_from, 'place_to': place_to}
-
-        group_list.append(group_dictionary)
-
-    response = json.dumps(
-        {'name': exhibition[0].NAME, 'description': exhibition[0].DESCRIPTION, 'museums': museums_list,
-         'expositions': expositions_list, 'exhibits': exhibits_list,
-         'person': {'fio': fio, 'id': groups[0].GUIDE.id}, 'groups': group_list}, ensure_ascii=False)
-
+    id = request.GET.get("id", 2)
+    response = return_json(id)
+    if response == "<h2>Not Found EXCURSION</h2>" or response == "<h2>Not Found Group With This EXCURSION</h2>":
+        return HttpResponseNotFound(response)
     return HttpResponse(response)
 
 
@@ -92,7 +59,7 @@ def view_list_groups_tour(request):
     excursions_id = request.GET.get("excursions_id", 1)
 
     # Получаем данные и проверяем на существование
-    group_list = GROUP.objects.get_queryset().filter(EXCURSION_ID=excursions_id).order_by('id')
+    group_list = GROUP.objects.get_queryset().filter(EXCURSION_ID=excursions_id).order_by('TIME')
     if not group_list.exists():
         return HttpResponseNotFound("<h2>Not Found GROUP</h2>")
 
@@ -263,3 +230,50 @@ def add_status_or_accreditation(request):
     status = message_email(msg_subject, body)
 
     return HttpResponse(status)
+
+
+def return_json(id: int):
+    # Получаем данные и проверяем на существование
+    exhibition = EXCURSION.objects.filter(id=id)
+    museums = exhibition[0].museum.all()
+    expositions = exhibition[0].exhibition.all()
+    exhibits = exhibition[0].exhibit.all()
+    groups = GROUP.objects.filter(EXCURSION_ID=exhibition[0].id)
+
+    if not exhibition.exists():
+        return "<h2>Not Found EXCURSION</h2>"
+
+    if not groups.exists():
+        return "<h2>Not Found Group With This EXCURSION</h2>"
+
+    museums_list = []
+    for museum in museums:
+        museums_list.append(museum.NAME)
+
+    expositions_list = []
+    for exposition in expositions:
+        expositions_list.append(exposition.NAME)
+
+    exhibits_list = []
+    for exhibit in exhibits:
+        exhibits_list.append(exhibit.NAME)
+
+    fio = groups[0].GUIDE.PASSPORT_ID.SURNAME + " " + groups[0].GUIDE.PASSPORT_ID.NAME + " " + groups[
+        0].GUIDE.PASSPORT_ID.PATRONYMIC
+
+    group_list = []
+    for group in groups:
+        place_from = str(group.PLACE_GATHERING.COUNTRY) + " " + str(group.PLACE_GATHERING.CITY) + " " + str(
+            group.PLACE_GATHERING.STREET) + " " + str(group.PLACE_GATHERING.HOUSE)
+        place_to = str(group.PLACE_TERMINATION.COUNTRY) + " " + str(group.PLACE_TERMINATION.CITY) + " " + str(
+            group.PLACE_TERMINATION.STREET) + " " + str(group.PLACE_TERMINATION.HOUSE)
+        group_dictionary = {'date': str(group.TIME), 'free_slots': group.NUMBER_SEATS, 'id': group.id,
+                            'place_from': place_from, 'place_to': place_to}
+
+        group_list.append(group_dictionary)
+
+    response = json.dumps(
+        {'name': exhibition[0].NAME, 'description': exhibition[0].DESCRIPTION, 'museums': museums_list,
+         'expositions': expositions_list, 'exhibits': exhibits_list,
+         'person': {'fio': fio, 'id': groups[0].GUIDE.id}, 'groups': group_list}, ensure_ascii=False)
+    return response
